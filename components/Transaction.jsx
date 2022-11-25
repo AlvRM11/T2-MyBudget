254b745e1f75a33b6eed0645b61339f96808e6bc
Álvaro <user2725@salesianos-lacuesta.net>
2022-11-25 00:30:13 +0000

    build: switching to previous branch

---
+++ components/Transaction.jsx
@@ -0,0 +1,27 @@
+import { Button, Pressable, StyleSheet, Text, View } from 'react-native'
+
+const Transaction = ({ info, removeTransaction }) => {
+    const {id, description, amount, date} = info;
+
+    return (
+      <View style={styles.container}>
+        <Text>{amount} - {description} - {date}</Text>
+        <Button title='Delete' onPress={() => removeTransaction({id})} />
+      </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        borderWidth: 2,
+        borderRadius: 20,
+        marginTop: 50,
+        padding: 20
+    }
+});
+
+export default Transaction;
